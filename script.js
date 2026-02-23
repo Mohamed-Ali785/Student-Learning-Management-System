e3271fdb9880955872a92dc7baf1fa5f5ac15750
Mohamed-Ali785 <eng.mohamed.ali.alhamaimy@gmail.com>
2026-02-23 03:30:12 +0200

    Start with login,register and dashboard pages

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // ===== Redirect if not logged in =====
-function checkLogin(){
-    if(!localStorage.getItem("username")){
+function checkLogin() {
+    if (!localStorage.getItem("username")) {
         window.location.href = "index.html";
     }
 }
 
-// ===== Logout Function =====
-function logout(){
-    localStorage.clear();
-    window.location.href = "index.html";
-}
 
 // ===== Clock =====
-function startClock(elementId){
-    function updateClock(){
+function startClock(elementId) {
+    function updateClock() {
         const now = new Date();
-        document.getElementById(elementId).innerText = now.toLocaleTimeString();
+        var clock = now.toLocaleTimeString();
+        document.getElementById(elementId).innerHTML = `<h2>Clock</h2> <div>${clock}</div>`;
     }
-    setInterval(updateClock,1000);
+    setInterval(updateClock, 1000);
     updateClock();
 }
+let users = JSON.parse(localStorage.getItem("users")) || [];
+let index = parseInt(localStorage.getItem("currentUserIndex"));
+document.getElementsByClassName("user")[0].innerText = `Hello ${users[index].name}✌️`;
